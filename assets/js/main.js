1f17da316a9d3a64591a5534c7529c2698b81f4d
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2024-09-10 14:20:08 -0600

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -314,16 +314,18 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("swiper-slide");
+	let i;
+	let slides = document.getElementsByClassName("swiper-slide");
   
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+
+	slideIndex++;
+
+	if (slideIndex > slides.length) {slideIndex = 1}
   
-  slides[slideIndex-1].style.display = "block";  
+	slides[slideIndex-1].style.display = "block";  
   
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 100000); // Change image every 2 seconds
 }
