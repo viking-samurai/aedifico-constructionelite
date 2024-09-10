3f4133cc95bf1af4996bf284aacf7c250815df8f
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2024-09-10 13:53:33 -0600

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -309,7 +309,21 @@ window.onclick = function(event){
 
 
 
-
-//Automate the carousel
+//Slideshow automation
 let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("swiper-slide");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
